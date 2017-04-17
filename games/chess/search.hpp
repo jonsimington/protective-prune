c04b6f3d201d11432c0ef90b027fbcfa8b753936
Shawn McCormick <wsmkd6@mst.edu>
2017-04-16 22:34:36 -0500

    Documentation

--- games/chess/search.hpp
+++ games/chess/search.hpp
@@ -22,9 +22,8 @@ namespace cpp_client
 
 namespace chess
 {
-	
-using hist = std::unordered_map<std::string, int>;
 
+using hist = std::unordered_map<std::string, int>;
 
 // Find the lowest possible value from all actions
 float minv(State& state, int depth, const Game& game, float alpha, float beta, int quiescence, hist& history);
@@ -32,13 +31,17 @@ float minv(State& state, int depth, const Game& game, float alpha, float beta, i
 // Find the highest possible value from all actions
 float maxv(State& state, int depth, const Game& game, float alpha, float beta, int quiescence, hist& history);
 
-// Perform Depth-limited Minimax Search, exploring to the target depth
+// Perform Depth-limited Minimax Search w/ quiescence search + history table
 // Parameters:
 //      Game& game: The game object
 //      State& current_state: The starting state
 //      int max_depth: The maximum depth to explore to
+//      float alpha: The best value available for the max player
+//      float beta: The best value available for the min player
+//      int quiescence: Number of quiescence-search depth increases allowed
+//      std::unordered_map<std::string, int> history: The move history table
 // Returns the best action found to take from the given state
-MyMove tlmm(const Game& game, State& current_state, int max_depth, int &best_value, int quiescence, hist& history);
+MyMove dlmm(const Game& game, State& current_state, int max_depth, int &best_value, int quiescence, hist& history);
 
 // Perform Time-limited Iterative deepening depth-limited alpha-beta pruning Minimax Search
 // Parameters:
@@ -46,6 +49,7 @@ MyMove tlmm(const Game& game, State& current_state, int max_depth, int &best_val
 //      State& current_state: The starting state
 //      int max_depth: The maximum depth to explore to
 //      int max_time: The maximim time to spend, in seconds
+//      int quiescence: Number of quiescence-search depth increases allowed
 // Returns the best action found to take from the given state 
 MyMove tliddlmm(const Game& game, State& current_state, int max_depth=15, int max_time=1, int quiescence=3);
 
